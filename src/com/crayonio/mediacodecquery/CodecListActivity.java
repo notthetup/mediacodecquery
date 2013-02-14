b53d3edcf883714a7562a37041a95e33dade2895
Chinmay Pendharkar <notthetup@gmail.com>
2013-02-14 22:28:45 +0800

    added MediaCodec printouts

--- src/com/crayonio/mediacodecquery/CodecListActivity.java
+++ src/com/crayonio/mediacodecquery/CodecListActivity.java
@@ -1,22 +1,40 @@
 package com.crayonio.mediacodecquery;
 
+import android.media.MediaCodecInfo;
+import android.media.MediaCodecList;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class CodecListActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_codec_list);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_codec_list);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_codec_list, menu);
-        return true;
-    }
-    
+		Log.i("CodecListActivity"," This device supports");
+		for (int codecIndex = 0; codecIndex < MediaCodecList.getCodecCount(); ++codecIndex) {
+			MediaCodecInfo thisCodec = MediaCodecList.getCodecInfoAt(codecIndex);
+			
+			String outputString = thisCodec.getName() + " as ";
+			
+			if (thisCodec.isEncoder()){
+				outputString += "an encoder";
+			}
+			else{
+				outputString += "a decoder";
+			}
+			Log.i("CodecListActivity", outputString);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_codec_list, menu);
+		return true;
+	}
+
 }
